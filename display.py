d1ed1134b7eb13b6b067df6b59806d2a2702a366
richard irving <richard.irving@engineering.digital.dwp.gov.uk>
2023-06-09 10:32:05 +0100

    feat: While loop with Q break, check for empty KeyName

--- display.py
+++ display.py
@@ -1,14 +1,18 @@
 from bcolours import bcolours as bc
 
 def display(asg, inst, indent=1):
-        ind = '\t'*indent
-        state = asg['State']
-        print(f"{ind}{asg['InstanceId']}{ind}{asg['InstanceType']}{ind}{inst['HealthStatus']}{ind}{ind}{inst['AvailabilityZone']}{ind}{asg['LaunchTime']}{ind}{asg['KeyName']}{ind}{state['Name']}")
+      ind = '\t'*indent
+      #   state = asg['State']
+
+      if 'KeyName' in asg:
+            print(f"{ind}{asg['InstanceId']}{ind}{asg['InstanceType']}{ind}{inst['HealthStatus']}{ind}{ind}{inst['AvailabilityZone']}{ind}{asg['LaunchTime']}{ind}{asg['State']['Name']}{ind}{ind}{asg['KeyName']}")
+      else:
+            print(f"{ind}{asg['InstanceId']}{ind}{asg['InstanceType']}{ind}{inst['HealthStatus']}{ind}{ind}{inst['AvailabilityZone']}{ind}{asg['LaunchTime']}{ind}{asg['State']['Name']}{ind}{ind}--")
 
 def setup(indent=1):
       ind = '\t'*indent
-      print(f"{bc.BOLD}{bc.OKGREEN}{ind}Instance Id{ind}{ind}Instance Type{ind}Health Status{ind}AZ{ind}{ind}Launch Date & Time{ind}{ind}Key Name{ind}{ind}State{bc.ENDC}")
+      print(f"{bc.BOLD}{bc.OKGREEN}{ind}Instance Id{ind}{ind}Instance Type{ind}Health Status{ind}AZ{ind}{ind}Launch Date & Time{ind}{ind}State{ind}{ind}Key Name{bc.ENDC}")
 
 
 if __name__ == '__main__':
-    display(self)
+    setup()
--- main.py
+++ main.py
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     else:
         if "AWS_PROFILE" in os.environ:
             print(f"{bc.OKBLUE}The script will continue with the currently set AWS_PROFILE.{bc.ENDC}")
-        else:
-            print(f"{bc.WARNING}AWS_PROFILE environment variable is not set.{bc.ENDC}\n{bc.FAIL}The script will exit.{bc.ENDC}")
-            quit()
+        # else:
+        #     print(f"{bc.WARNING}AWS_PROFILE environment variable is not set.{bc.ENDC}\n{bc.FAIL}The script will exit.{bc.ENDC}")
+        #     quit()
     
     # Assign profile to environ var
     os.environ['AWS_PROFILE'] = profile_name
@@ -47,72 +47,92 @@ if __name__ == '__main__':
     # # Output bucket names
     # for bucket in response['Buckets']:
     #     print(f'    {bucket["Name"]}')
-    asg = 'integration-production-egress-waf'
-    # asg = input(f'{bc.OKBLUE}Please input the ASG name you are working with : {bc.ENDC}')
-    try:
-        asg_client = boto3.client('autoscaling', region_name='eu-west-2')
-    except ClientError as e:
-        print(e)
-        print(f"{bc.FAIL} There may be an issue with your credentials{bc.ENDC}")
-        sys.exit(1)
-
-    try:
-        ec2_client = boto3.client('ec2', region_name='eu-west-2')
-    except ClientError as e:
-        print(e)
-        print(f"{bc.FAIL} There may be an issue with your credentials{bc.ENDC}")
-        sys.exit(1)
-
-    try:
-        asg_response = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg])
-    except Exception as ex:
-        print(f"Error - {ex}")
-        print(f"{bc.FAIL} There may be an issue with the ASG name you entered{bc.ENDC}")
-        sys.exit(1)
-
-
-
     
-    # dict_you_want = {key: old_dict[key] for key in your_keys}
-    # listkeys = ['AutoScalingGroupName', 'MinSize' ]
-    # asg_dict = {key: asg_response['AutoScalingGroups'][0] for key in listkeys}
-    instance_ids = []
-    instance_vals = []
-    asg_dict = {}
-    inst_dict = {}
+    #Assign AutoScalingGroup name to query
+    # # asg = 'integration-production-egress-waf'
+    #  Not recognised as an asg = 'production-squid-proxy-ec2-messages'
+    # asg = 'integration-stage-iig-waf'
+    # Empty KeyName - asg = 'production-grafana'
+    asg = input(f'{bc.OKBLUE}Please input the ASG name you are working with : {bc.ENDC}')
 
-    for i in asg_response['AutoScalingGroups']:
-        asg_dict = {
-            'AutoScalingGroupName': i['AutoScalingGroupName'],
-            'MinSize': i['MinSize'],
-            'MaxSize': i['MaxSize'],
-            'DesiredCapacity': i['DesiredCapacity']
-        }
-        for k in i['Instances']:
-            inst_dict = {
-                'InstanceId': k['InstanceId'],
-                'InstanceType': k['InstanceType'],
-                'HealthStatus': k['HealthStatus'],
-                'AvailabilityZone': k['AvailabilityZone']
-            }
-            instance_vals.append(inst_dict)
-            instance_ids.append(k['InstanceId'])
-    
-    asg_dict['Instances'] = instance_vals
-    
-    print(f"  {bc.OKCYAN} ASG group set to : {asg} {bc.ENDC} {bc.WARNING}Scaling Min/Desired/Max: {asg_dict['MinSize']}/{asg_dict['DesiredCapacity']}/{asg_dict['MaxSize']}{bc.ENDC}")
-    display.setup()
+    while 1:
+        try:
+            asg_client = boto3.client('autoscaling', region_name='eu-west-2')
+        except ClientError as e:
+            print(e)
+            print(f"{bc.FAIL} There may be an issue with your credentials{bc.ENDC}")
+            sys.exit(1)
 
-    ec2_response = ec2_client.describe_instances(
-        InstanceIds = instance_ids
-        )
-    
-    for i in ec2_response['Reservations']:
-        for k in i['Instances']:
-            # display.display(k)
-            for j in asg_dict['Instances']:
-                if k['InstanceId'] == j['InstanceId']:
-                    display.display(k,j)
+        try:
+            ec2_client = boto3.client('ec2', region_name='eu-west-2')
+        except ClientError as e:
+            print(e)
+            print(f"{bc.FAIL} There may be an issue with your credentials{bc.ENDC}")
+            sys.exit(1)
+
+        try:
+            asg_response = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg])
+        except Exception as ex:
+            print(f"Error - {ex}")
+            print(f"{bc.FAIL} There may be an issue with the ASG name you entered{bc.ENDC}")
+            sys.exit(1)
+
+
+
+        
+        # dict_you_want = {key: old_dict[key] for key in your_keys}
+        # listkeys = ['AutoScalingGroupName', 'MinSize' ]
+        # asg_dict = {key: asg_response['AutoScalingGroups'][0] for key in listkeys}
+        instance_ids = []
+        instance_vals = []
+        asg_dict = {}
+        inst_dict = {}
+
+        if len(asg_response['AutoScalingGroups']) != 0:
+            for i in asg_response['AutoScalingGroups']:
+                asg_dict = {
+                    'AutoScalingGroupName': i['AutoScalingGroupName'],
+                    'MinSize': i['MinSize'],
+                    'MaxSize': i['MaxSize'],
+                    'DesiredCapacity': i['DesiredCapacity']
+                }
+                for k in i['Instances']:
+                    inst_dict = {
+                        'InstanceId': k['InstanceId'],
+                        'InstanceType': k['InstanceType'],
+                        'HealthStatus': k['HealthStatus'],
+                        'AvailabilityZone': k['AvailabilityZone']
+                    }
+                    instance_vals.append(inst_dict)
+                    instance_ids.append(k['InstanceId'])
+            
+            asg_dict['Instances'] = instance_vals
+            
+            print(f"  {bc.OKCYAN} ASG group set to : {bc.ENDC}{bc.WARNING}{bc.BOLD}{asg}{bc.ENDC}{bc.OKCYAN} \t Scaling Min/Desired/Max: {bc.ENDC}{bc.WARNING}{bc.BOLD}{asg_dict['MinSize']}/{asg_dict['DesiredCapacity']}/{asg_dict['MaxSize']}{bc.ENDC}")
+            display.setup()
+
+            ec2_response = ec2_client.describe_instances(
+                InstanceIds = instance_ids
+                )
+            
+            for i in ec2_response['Reservations']:
+                for k in i['Instances']:
+                    # display.display(k)
+                    for j in asg_dict['Instances']:
+                        if k['InstanceId'] == j['InstanceId']:
+                            display.display(k,j)
+        
+            try:
+                loop_resp = input("\nPress any key to recheck ASG or Q to exit\n")
+            except SyntaxError:
+                pass
+
+            if loop_resp == 'Q':
+                break
+        else:
+            print("There seems to be an issue with the ASG group provided.")
+            break
+
 
 
 

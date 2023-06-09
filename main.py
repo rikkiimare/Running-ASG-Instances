5a08528bf399d56183fc3faf0385e7f1de3a8a6f
richard irving <richard.irving@engineering.digital.dwp.gov.uk>
2023-06-09 15:28:12 +0100

    Set sso credentials to specify a profile to use

--- main.py
+++ main.py
@@ -10,35 +10,40 @@ import display
 
 if __name__ == '__main__':
     
-    # identify all account profiles within local credentials file
-    profile_name = find_profiles_in_credentials.find_in_cred_file()
+    # TO LINE 43 COMMENTED OUT
+    # # # identify all account profiles within local credentials file
+    # profile_name = find_profiles_in_credentials.find_in_cred_file()
 
-    # Display the last modified time to the screen
-    amend_aws_cred.time_cred_file_mod()
+    # # Display the last modified time to the screen
+    # amend_aws_cred.time_cred_file_mod()
 
-    # Call module to accept credentials from user
-    yes_choices = ['yes', 'y']
+    # # Call module to accept credentials from user
+    # yes_choices = ['yes', 'y']
 
-    user_resp = input(f'{bc.OKBLUE}Do you want to amend the local .aws\credentials file? (yes/no): {bc.ENDC}')
-    if user_resp.lower() in yes_choices:
-        creds = amend_aws_cred.accept_creds()
+    # user_resp = input(f'{bc.OKBLUE}Do you want to amend the local .aws\credentials file? (yes/no): {bc.ENDC}')
+    # if user_resp.lower() in yes_choices:
+    #     creds = amend_aws_cred.accept_creds()
 
-        # Remove the old credentials out of the ~/.aws/credentials file
-        amend_aws_cred.rm_cred_from_env(creds)
+    #     # Remove the old credentials out of the ~/.aws/credentials file
+    #     amend_aws_cred.rm_cred_from_env(creds)
 
-        # Add new credentials provided into the ~/.aws/credentials file
-        profile_name = amend_aws_cred.set_cred_from_env(creds)
+    #     # Add new credentials provided into the ~/.aws/credentials file
+    #     profile_name = amend_aws_cred.set_cred_from_env(creds)
         
-    else:
-        if "AWS_PROFILE" in os.environ:
-            print(f"{bc.OKBLUE}The script will continue with the currently set AWS_PROFILE.{bc.ENDC}")
-        # else:
-        #     print(f"{bc.WARNING}AWS_PROFILE environment variable is not set.{bc.ENDC}\n{bc.FAIL}The script will exit.{bc.ENDC}")
-        #     quit()
+    # else:
+    #     if "AWS_PROFILE" in os.environ:
+    #         print(f"{bc.OKBLUE}The script will continue with the currently set AWS_PROFILE.{bc.ENDC}")
+    #     # else:
+    #     #     print(f"{bc.WARNING}AWS_PROFILE environment variable is not set.{bc.ENDC}\n{bc.FAIL}The script will exit.{bc.ENDC}")
+    #     #     quit()
     
-    # Assign profile to environ var
-    os.environ['AWS_PROFILE'] = profile_name
-    print(f"\n{bc.HEADER} {os.environ['AWS_PROFILE']} {bc.ENDC}")
+    # # Assign profile to environ var
+    # os.environ['AWS_PROFILE'] = profile_name
+    # print(f"\n{bc.HEADER} {os.environ['AWS_PROFILE']} {bc.ENDC}")
+    # FROM LINE 13 COMMENTED OUT
+
+    # Set sso credentials
+    boto3.setup_default_session(profile_name='072874872668_SreOnCall')
 
     # Works - commented out while working on the ~/.aws/credentials file
     # s3 = boto3.client('s3')
